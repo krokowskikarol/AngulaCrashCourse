a4147855612756ea9c0a7568480f0425c6fbd952
Karol Krokowski <karol.krokowski@Aliplast.pl>
2022-08-28 20:06:41 +0200

    second commit

---
+++ src/app/http.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private _http: HttpClient) { }
+
+  getBrew() {
+    return this._http.get('https://api.openbrewerydb.org/breweries');
+  }
+}
